98fab4dd04ceb54d9777a723b7cae8860935bada
Leonty Belskiy <prishelec@gmail.com>
2014-08-25 23:52:49 +0200

    Move order form logic in the directive, use directive scope

--- app/scripts/controllers/orders.js
+++ app/scripts/controllers/orders.js
@@ -4,21 +4,6 @@ angular.module('estesWebApp').controller('OrdersCtrl', ['$scope', '$interval', '
                                                 function($scope, $interval, Order, Waiter, Dish) {
 
 	var cols = 4;
-	$scope.waiterList = null;
-	$scope.searchTerm = "";
-	$scope.dishList = null;
-	$scope.dishListExpanded = false;
-	$scope.orderFormDish = null;
-	$scope.addingNewDish = false;
-	
-	var OrderTemplate = function(waiter) {
-		return {
-			status: 'PREPARATION',
-			waiter: waiter,
-			dishes: [],
-			note: null
-		}
-	}
 	
 	var refreshOrders = function() {
 		Order.readAll().then(function(orders) {
@@ -30,62 +15,6 @@ angular.module('estesWebApp').controller('OrdersCtrl', ['$scope', '$interval', '
 	}
 	
 	refreshOrders();
-
-	Waiter.readAll().then(function(waiters) {
-		$scope.waiterList = waiters;
-		$scope.order = OrderTemplate(waiters[0]);
-	});
-
-	Dish.readAll().then(function(dishList) {
-		$scope.dishList = dishList;
-	});		
-	
-	Dish.readAllMenus().then(function(menus) {
-		$scope.menus = menus;
-		$scope.selectedMenus = angular.copy($scope.menus);	
-	});	
-	
-	$scope.filterDish = function(dish) {
-    	var menuFilter = _.some(dish.menus, function(menu) {
-    		return $scope.selectedMenus.indexOf(menu) != -1;
-    	});	
-    
-    	function isInSearch(dish) {
-    		return $scope.searchTerm.length > 0 ? dish.name.toUpperCase().indexOf($scope.searchTerm.toUpperCase()) != -1: true;
-    	}
-    	
-    	return menuFilter && isInSearch(dish);
-	}	
-	
-	$scope.waiterToLabel = function(waiter) {
-		return waiter.name + ' (' + waiter.id + ')';
-	}
-	
-	$scope.showAddDish = function() {
-		$scope.addingNewDish = true;
-	}
-	
-	$scope.hideAddDish = function() {
-		$scope.addingNewDish = false;
-	}
-	
-	$scope.toggleDishList = function() {
-		$scope.dishListExpanded = !$scope.dishListExpanded;
-	}
-	
-	$scope.configDish = function(dish) {		
-		$scope.orderFormDish = Order.toOrderDish(dish);
-	}
-	
-	$scope.addDishToOrder = function(dish) {
-		$scope.order.dishes.push(dish);
-		$scope.orderFormDish = null;
-		$scope.addingNewDish = false;
-	}
-	
-	$scope.cancelAddingDish = function() {
-		$scope.orderFormDish = null;
-	}
 	
 	$scope.calculateTotal = function(order) {
 		if (!order) return 0;
@@ -101,19 +30,8 @@ angular.module('estesWebApp').controller('OrdersCtrl', ['$scope', '$interval', '
 		$scope.order = angular.copy(order);
 	}
 	
-	$scope.save = function(order) {
-		resetOrder();
-		Order.save(order).then(function() {
-			refreshOrders();
-		});
-	}
-	
-	$scope.cancel = function() {
-		resetOrder();
-	}
-	
-	function resetOrder() {
-		$scope.order = OrderTemplate($scope.waiterList[0]);
+	$scope.onOrderSave = function() {
+		refreshOrders();
 	}
 	
 	var ordersPoll = $interval(refreshOrders, 2000);
--- app/scripts/directives/order-form.js
+++ app/scripts/directives/order-form.js
@@ -1,11 +1,114 @@
 'use strict';
 
-angular.module('estesWebApp').directive('orderForm', function() {
+angular.module('estesWebApp').directive('orderForm', ['Waiter', 'Dish', 'Order', 
+                                              function(Waiter, Dish, Order) {
+
+	var OrderTemplate = function(waiter) {
+		return {
+			status: 'PREPARATION',
+			waiter: waiter,
+			dishes: [],
+			note: null
+		}
+	}
+	
 	return {
 		templateUrl : 'views/directives/order-form.html',
 		replace: true,
 		restrict : 'E',
-		link : function postLink(scope, element, attrs) {
+		scope: {
+			order: '=',
+			onSave: '&'
+		},
+		link : function postLink($scope, element, attrs) {
+			
+			$scope.waiterList = null;
+			$scope.searchTerm = "";
+			$scope.dishList = null;
+			$scope.dishListExpanded = false;
+			$scope.orderFormDish = null;
+			$scope.addingNewDish = false;
+			
+			Waiter.readAll().then(function(waiters) {
+				$scope.waiterList = waiters;
+				$scope.order = OrderTemplate(waiters[0]);
+			});
+
+			Dish.readAll().then(function(dishList) {
+				$scope.dishList = dishList;
+			});		
+			
+			Dish.readAllMenus().then(function(menus) {
+				$scope.menus = menus;
+				$scope.selectedMenus = angular.copy($scope.menus);	
+			});	
+			
+			$scope.filterDish = function(dish) {
+		    	var menuFilter = _.some(dish.menus, function(menu) {
+		    		return $scope.selectedMenus.indexOf(menu) != -1;
+		    	});	
+		    
+		    	function isInSearch(dish) {
+		    		return $scope.searchTerm.length > 0 ? dish.name.toUpperCase().indexOf($scope.searchTerm.toUpperCase()) != -1: true;
+		    	}
+		    	
+		    	return menuFilter && isInSearch(dish);
+			}	
+			
+			$scope.waiterToLabel = function(waiter) {
+				return waiter.name + ' (' + waiter.id + ')';
+			}
+			
+			$scope.showAddDish = function() {
+				$scope.addingNewDish = true;
+			}
+			
+			$scope.hideAddDish = function() {
+				$scope.addingNewDish = false;
+			}
+			
+			$scope.toggleDishList = function() {
+				$scope.dishListExpanded = !$scope.dishListExpanded;
+			}
+			
+			$scope.configDish = function(dish) {		
+				$scope.orderFormDish = Order.toOrderDish(dish);
+			}
+			
+			$scope.addDishToOrder = function(dish) {
+				$scope.order.dishes.push(dish);
+				$scope.orderFormDish = null;
+				$scope.addingNewDish = false;
+			}
+			
+			$scope.cancelAddingDish = function() {
+				$scope.orderFormDish = null;
+			}
+			
+			$scope.calculateTotal = function(order) {
+				if (!order) return 0;
+				
+				var total = 0;
+				_.each(order.dishes, function(dish) {
+					total += Dish.getPrice(dish);
+				});
+				return total;
+			}
+			
+			$scope.save = function(order) {
+				resetOrder();
+				Order.save(order).then(function() {
+					$scope.onSave();
+				});
+			}
+			
+			$scope.cancel = function() {
+				resetOrder();
+			}
+			
+			function resetOrder() {
+				$scope.order = OrderTemplate($scope.waiterList[0]);
+			}
 		}
 	};
-});
+}]);
