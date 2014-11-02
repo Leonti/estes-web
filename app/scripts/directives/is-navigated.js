1a48130a2b0a8d7b699df1478b110fe55672c251
Leonti Bielski <prishelec@gmail.com>
2014-11-02 11:27:21 +0100

    Add some styling to the header

---
+++ app/scripts/directives/is-navigated.js
@@ -0,0 +1,32 @@
+'use strict';
+
+angular.module('estesWebApp').directive('isNavigated', ['$rootScope', '$location', function($rootScope, $location) {
+	return {
+		restrict : 'A',
+		link : function postLink(scope, element, attrs) {
+			
+			console.log('loaded');
+			
+			var url = null;
+
+			function checkStatus() {
+                if ($location.url().indexOf(url.replace('#', '')) !== -1) {
+                	angular.element(element).addClass('active');
+                } else {
+                	angular.element(element).removeClass('active');
+                }
+			}
+			
+			$rootScope.$on('$locationChangeSuccess', function(event, newUrl, oldUrl) {
+				if (url) {
+					checkStatus();
+				}
+			});			
+			
+            attrs.$observe('href', function(href) {
+                url = href;
+                checkStatus();
+            });
+		}
+	};
+}]);
